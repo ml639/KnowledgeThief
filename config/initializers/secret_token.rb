e7ef813948b59eface0d49a181f222bc1016f0ef
HNH <hoa801@gmail.com>
2012-10-08 16:15:42 -0600

    Another commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KnowledgeThief::Application.config.secret_token = '032d5fb0cf0572c533d69abe300e6d86520a0ec6976efe1716a6656435fc94fe4a68ebb11543312c714e083fb1f8187aed0a34980a495b9ada090b522573e1a0'
