43cfa182218e7b6d8963f01e9c020c191a56cfe6
HNH <hoa801@gmail.com>
2012-10-19 20:38:34 -0600

    fixed bug

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-config.assets.compile = true
 
+KnowledgeThief::Application.configure do
+   config.assets.compile = true
+end
 # Initialize the rails application
 Kt::Application.initialize!
