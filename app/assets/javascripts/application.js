9e488538a685fc82d272528eb0a9f3a5b7040ac0
HNH <hoa801@gmail.com>
2012-10-26 01:10:25 -0600

    fixed login stuff and icons

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,3 +22,13 @@ $(document).ready(function()
         e.stopPropagation();
         });
   });
+
+$(document).ready(function(){
+	$('#login-trigger').click(function(){
+		$(this).next('#login-content').slideToggle();
+		$(this).toggleClass('active');					
+
+		if ($(this).hasClass('active')) $(this).find('span').html('&#x25B2;')
+			else $(this).find('span').html('&#x25BC;')
+		})
+});
