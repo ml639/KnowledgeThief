68e3b903d8144874c2a77686c4c7cb65652862b4
HNH <hoa801@gmail.com>
2012-11-15 20:42:30 -0700

    Fixed Double Iframe bug

--- app/assets/javascripts/ResourceView.js
+++ app/assets/javascripts/ResourceView.js
@@ -5,11 +5,12 @@ var ResourceView = function(){
 		theIframe2.setAttribute("src", url);
 		theIframe2.style.width = "100%";
 		theIframe2.style.height = "100%";
-		theIframe2.style.display = "block"
-		theIframe2.style.position = "absolute";
+		theIframe2.style.border = "none";
 		theIframe2.style.top = "-1000px";
 		var containerDiv2 = document.getElementById('wrapper');
-		containerDiv2.appendChild(theIframe2);
+		$("#frameHolder").append(theIframe2);
+		$("#frameHolder").fadeIn("fast");
+		//containerDiv2.appendChild(theIframe2);
 		$("#resourceViewMenu").fadeIn("fast");
 		$("#resourceViewMenu").animate(
 			   {"top": "0px"},"slow");
@@ -40,6 +41,7 @@ $(function(){
 	});
 	$('#home_Button').click(function(){
 		rView.remove();
+		$("#frameHolder").fadeOut("fast");
 		$('#contentColumn').fadeIn();
 		$("#resourceViewMenu").animate(
 			   {"top": "-50px"},"slow");
