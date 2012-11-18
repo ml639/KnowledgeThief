678b45e12188faa3e6f99303cb2da11ac18cd127
HNH <hoa801@gmail.com>
2012-11-17 20:36:19 -0700

    added jquery UI demo for sorting tabs

--- app/assets/javascripts/modalForms.js
+++ app/assets/javascripts/modalForms.js
@@ -1,10 +1,33 @@
 $(document).ready(function() {
 	$('.modalPopup').fancybox({
-	    maxWidth    : 400,
-	    maxHeight   : 400,
+	    maxWidth    : 300,
+	    maxHeight   : 250,
 	    fitToView   : true,
 	    autoSize    : false,
 	    openEffect  : 'none',
 	    closeEffect : 'none',
+	});
+});
+
+$(document).ready(function() {
+	$('.modalPopup_resource').fancybox({
+	    maxWidth    : 300,
+	    maxHeight   : 350,
+	    fitToView   : true,
+	    autoSize    : false,
+	    openEffect  : 'none',
+	    closeEffect : 'none',
+	});
+});
+
+$(document).ready(function() {
+	$('.modalPopup_question').fancybox({
+	    maxWidth    : 300,
+	    maxHeight   : 250,
+	    fitToView   : true,
+	    autoSize    : false,
+	    openEffect  : 'none',
+	    closeEffect : 'none',
+		scrolling   : 'no',
 	});
 });
