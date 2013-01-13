fe234a36d8d2724d4cd7d6ab0c1219af4bf3dadf
Landon Wilkins <landonwilkins@gmail.com>
2013-01-13 09:09:36 -0700

    Add additional comments in home#index, as well as remove extraneous if/else statement, which is a remnant of resources#index.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,21 @@
 class HomeController < ApplicationController
   def index
-      if params[:tag] && params[:tag] != ""
-        @resources = Resource.tagged_with(params[:tag]).find_with_reputation(:votes, :all, order: "votes desc")
-      else
-        @resources = Resource.find_with_reputation(:votes, :all, :limit => 10, order: "votes desc")
-      end
+  
+      # 
+      #if params[:tag] && params[:tag] != ""
+      #  @resources = Resource.tagged_with(params[:tag]).find_with_reputation(:votes, :all, order: "votes desc")
+      #else
+      #  @resources = Resource.find_with_reputation(:votes, :all, :limit => 10, order: "votes desc")
+      #end
+      
+      
       @resource = Resource.new
       
-      # @resources = (find recent)
-      # @resources = (find recommended)
+      # @resources = Popular resources
+      @resources = Resource.find_with_reputation(:votes, :all, :limit => 10, order: "votes desc")
+      
+      # @resources = Recent resources (pending User Activity gem)
+      
+      # @resources = Recommended resources (pending User Activity gem)
   end
 end
