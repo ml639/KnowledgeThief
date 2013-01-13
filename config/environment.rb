46bcce1760a7c0e6b384d7ea304399136ebc9d70
Landon Wilkins <landonwilkins@gmail.com>
2013-01-13 15:31:47 -0700

    Fix character encoding error concerning UTF-8 and ASCII-8BIT

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Kt::Application.initialize!
+
+# Ensure encoding is UTF 8
+Encoding.default_external = Encoding::UTF_8
+
+Encoding.default_internal = Encoding::UTF_8
