381e7fdc9d41f00f60d56d71e7a3318d1a98d52d
HNH <hoa801@gmail.com>
2013-01-13 17:27:44 -0700

    ajax upvotes

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -14,11 +14,15 @@ class ResourcesController < ApplicationController
     @resource.save
     redirect_to resources_path
   end
+  
   def vote
     value = params[:type] == "up" ? 1 : -1
     @resource = Resource.find(params[:id])
     @resource.add_or_update_evaluation(:votes, value, current_user)
-    redirect_to :back, notice: "Thank you for voting"
+    respond_to do |format|  
+        format.html { redirect_to :back, notice: "Thank you for voting" }  
+        format.json { render :status=>200, :json=>{:success=>true}}  
+    end
   end
   
   def isYoutube(youtube_url)
